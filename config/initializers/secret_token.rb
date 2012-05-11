6d5189449453b4fc77b1c497cf2f619e0bd0eb49
Rinus Loof <rinux@xloof.nl>
2012-05-11 19:54:11 +0200

    startsituatie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '69df1513898059e60a5fc996175b615737c9d37f2ce96d2de96fda88379604bdf37da2c45b56b05baa45a02d82bc49f8cdbf03a1b4bb3ed9ca17e3f08deb58b4'
